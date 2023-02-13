6227810cebbdac229a70915baec3246279507643
claraortiz <clara.ortizdelatorre@gmail.com>
2023-02-13 12:30:34 +0100

    :sparkles: pagina principal

--- src/pages/property-list/property-list.api.js
+++ src/pages/property-list/property-list.api.js
@@ -1,10 +1,21 @@
 import Axios from "axios";
 
 
-const url = `${process.env.BASE_API_URL}/properties`;
 
-export const getPropertyList = () => 
-    Axios.get(url).then(({ response }) => {
-        return response.data;
-    });
-   
+const propertyListUrl = `${process.env.BASE_API_URL}/properties?`;
+
+export const getPropertyList = queryParams =>
+  Axios.get(`${propertyListUrl}${queryParams}`).then(({ data }) => data);
+
+  //lista types
+const saleTypeListUrl = `${process.env.BASE_API_URL}/saleTypes`;
+
+export const getSaleTypeList = () =>
+  Axios.get(saleTypeListUrl).then(({ data }) => data);
+
+
+//lista de provincias
+const provinceListUrl = `${process.env.BASE_API_URL}/provinces`;
+
+export const getProvinceList = () =>
+  Axios.get(provinceListUrl).then(({ data }) => data);
